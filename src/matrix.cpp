aebcfcf53b8965a37c302dc50a92f080167bdfd3
Ioannis Skoulidas <io.a.skoul@gmail.com>
2019-04-18 13:41:03 -0400

    Cool

---
+++ src/matrix.cpp
@@ -0,0 +1,8 @@
+#include "matrix.hpp"
+
+matrix::matrix(std::initializer_list<std::initializer_list<double>> in) {
+    data.resize(in.size());
+    for(auto &v : data) v.resize(in.begin.size());
+    
+    for(int i = 0; i < data.size())
+}
--- src/matrix.hpp
+++ src/matrix.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+
 class matrix {
     
+public:
+    matrix(std::initializer_list<std::initializer_list<double>> in);
+
+    
+
+private:
+    std::vector<std::vector<double>> data;
 };
